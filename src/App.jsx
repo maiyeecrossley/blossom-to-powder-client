dd3fc3fa3b3f25f9bbc908f9abf3da833f751f06
maiyeecrossley <maiyee.crossley@gmail.com>
2025-03-03 23:26:30 +0000

    cleaned up all locations component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./components/Register/Register"
 import Login from "./components/Login/Login"
 import SeasonalLocations from './components/SeasonalLocations/SeasonalLocations'
 import Seasons from './components/Seasons/Seasons'
+import AllLocations from './components/AllLocations/AllLocations'
 
 function App() {
   
@@ -17,12 +18,14 @@ function App() {
       <NavLink to = "/auth/register/">Register </NavLink>
       <NavLink to = "/auth/login/">Login </NavLink>
       <NavLink to = "/seasons/">Explore Seasons </NavLink>
+      <NavLink to = "/locations/">All Locations </NavLink>
 
     </nav>
     <main>
       <Routes>
         <Route path="/auth/register/" element={<Register />} />
         <Route path="/auth/login/" element={<Login />} />
+        <Route path="/locations/" element={<AllLocations />} />
         <Route path="/seasons/:seasonId/locations/" element={<SeasonalLocations />} />
         <Route path="/seasons/" element={<Seasons />} />
       
--- src/services/locationService.js
+++ src/services/locationService.js
@@ -4,6 +4,16 @@ import { getToken } from "../utils/auth.js"
 const BASE_URL = import.meta.env.VITE_API_URL
 
 
+export const locationIndex = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/locations/`)
+        return response.data
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
 export const seasonLocationIndex = async (seasonId) => {
     
     try {
